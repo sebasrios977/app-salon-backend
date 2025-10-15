1197ba48f0e7814355cee01c45309298b8f97e48
sebasrios97 <sebastianriosrico@gmail.com>
2025-10-15 18:03:47 -0500

    42th section completed

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -9,14 +9,14 @@ export async function sendEmailVerification({ name, email, token }) {
   );
 
   const info = await transport.sendMail({
-    from: "AppSalon",
+    from: "AppSalon <cuentas@appsalon.com>",
     to: email,
     subject: "AppSalon - Confirma tu cuenta",
     text: "AppSalon - Confirma tu cuenta",
     html: `
         <p>Hola: ${name}, confirma tu cuenta en AppSalon</p>
         <p>Tu cuenta ya está casi lista, solo debes comprobarla en el siguiente enlace:
-        <a href="http://localhost:4000/api/auth/verify/${token}">Confirmar cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
     `,
   });
